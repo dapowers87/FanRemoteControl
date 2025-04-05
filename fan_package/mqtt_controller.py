49c3f14dd86bca896289652f5943740bd94d4f2e
David Powers <dapowers87@gmail.com>
2025-04-05 12:04:27 -0500

    Update mqtt_controller.py

--- fan_package/mqtt_controller.py
+++ fan_package/mqtt_controller.py
@@ -1,7 +1,5 @@
 import paho.mqtt.client as mqtt
 import time
-from ping3 import ping
-import socket
 
 class MqttController:
     
@@ -9,8 +7,6 @@ class MqttController:
     __message_parser = None
     __mqtt_client = None
     connected = False
-    host = "m93p.local" 
-    # host = "192.168.86.42"
     
     def __init__(self, message_parser):
         self.__message_parser = message_parser
@@ -24,29 +20,6 @@ class MqttController:
     def on_message(self, client, userdata, message):
         self.__message_parser(message)
 
-    def ping_port(self, host, port):
-        while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.settimeout(5)  # Timeout after 5 seconds
-                try:
-                    sock.connect((host, port))
-                    print(f"{host}:{port} is online!")
-                    return  # Exit the loop once the port is reachable
-                except socket.error:
-                    print(f"{host}:{port} is still offline. Retrying in 5 seconds...")
-                    time.sleep(5)
-
-    def wait_for_host_to_be_online(self):
-        while True: # wait until a ping is returned from the host
-            if ping(self.host) is not None:
-                print(f"{self.host}: is reachable via ping!")
-                break
-            else:
-                print(f"{self.host}: is not reachable via ping. Waiting 5 seconds...")
-                time.sleep(5)
-
-        self.ping_port(self.host, 1883)
-
     def connect_to_mqtt(self):        
         print('Connecting to MQTT')
         self.connected = False
@@ -54,16 +27,14 @@ class MqttController:
         self.__mqtt_client.username_pw_set("fanPi", "F@nP!")
         self.__mqtt_client.on_message = self.on_message
         self.__mqtt_client.on_connect = self.on_connect
-        self.__mqtt_client.on_disconnect = self.on_disconnect 
-
-        self.wait_for_host_to_be_online()
-        
-        self.__mqtt_client.connect(self.host, 1883)
+        self.__mqtt_client.on_disconnect = self.on_disconnect
+#        self.__mqtt_client.connect("192.168.86.42", 1883)
+        self.__mqtt_client.connect("m93p.lan", 1883)
         self.__mqtt_client.loop_start()       #connect to broker
 
         while not self.connected:    #Wait for connection
-            time.sleep(1)
-
+            time.sleep(0.25)
+            
         print('Connected to MQTT')
 
         self.set_subscription()
@@ -73,13 +44,10 @@ class MqttController:
                 
         timeout = 1
         while not self.connected:    #Wait for connection
+            reattemptConnect = True # Initialize here
             try:
-                self.wait_for_host_to_be_online()
-                
                 if reattemptConnect:
                     self.__mqtt_client.reconnect()
-
-                break
             except:
                 reattemptConnect = True
                 print("reconnect failed")
@@ -91,8 +59,6 @@ class MqttController:
             if timeout > 60:
                 timeout = 60
 
-            if(self.connected == True):
-                print("Reconnected...")
         self.set_subscription()
 
     def set_subscription(self):
